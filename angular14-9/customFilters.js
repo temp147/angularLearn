09198d4841ef29368b9014b21e5516af2d9145f0
temp147 <temp147@163.com>
2015-09-14 18:29:19 +0800

    new

---
+++ angular14-9/customFilters.js
@@ -0,0 +1,15 @@
+/**
+ * Created by root on 8/11/15.
+ */
+
+angular.module("exampleApp")
+    .filter("labelCase",function(){
+        return function(value,reverse){
+            if(angular.isString(value)){
+                var intermediate
+            }else{
+
+            }
+
+        }
+    });
